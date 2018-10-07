79d2d4fb8851bde594a0ab9eee14b312aa1a8b43
jaswantp <jaswantphysx@gmail.com>
2018-10-08 01:08:05 +0530

    added comments

--- vel_dist.cpp
+++ vel_dist.cpp
@@ -5,6 +5,7 @@
 #include <cmath>
 VelDist::VelDist()
 {
+	//set seed, beam velocity, number of ions/electrons and thermal spread.
 	srand(time(NULL));
 	v_th = 2;
 	n_0  = 2000;
@@ -36,8 +37,11 @@ int VelDist::setn_0(int n0)
 }
 void VelDist::setVbounds()
 {
-	v_max = 3.*v_b;
-	v_min = -3.*v_b;
+	//v_max = allowable velocity of the farther edge of maxwellian with high velocity.
+	//v_min = allowable velocity of the lower edge of maxwellian with low velocity.
+	// DO NOT SET THE FACTOR = 1. SET IT SOMEWHERE B/W 1.1 AND 10.
+	v_max = 4*v_b;
+	v_min = -4*v_b;
 }
 void VelDist::show()
 {
@@ -50,6 +54,7 @@ void VelDist::show()
 }
 double VelDist::generateV()
 {
+	//generate random velocity b/w v_min and v_max.
 	v = v_min + (v_max - v_min)*(double(rand())/double(RAND_MAX));
 	//return v;
 	return generateF(v);
@@ -60,17 +65,23 @@ double VelDist::generateF(double v)
 	double param2 = -(v-v_b) * (v-v_b)/(v_th * v_th * 2.);
 	double param3 = -(v+v_b) * (v+v_b)/(v_th * v_th * 2.);
 	double F = (n_0 * 0.5) * sqrt(param1) * (exp(param2) + exp(param3)) ;
+
+	//FMAX at v = +v_b or -v_b.
 	double F_MAX = (n_0 * 0.5) * sqrt(param1) * (1 + exp(double((-2. * v_b * v_b)/(v_th*v_th))));
+
+	//generate random w within 0, F_MAX.
 	double w =  F_MAX * (double(rand())/double(RAND_MAX));
 	return acceptV(w,v,F);
 }
 double VelDist::acceptV(double w, double v, double F)
 {
+	//acceptance of v.
 	if (w < F)
 	{
 		std::cout << F << ",";
 		return v;
 	}
+	//Rejection and regenerate v.
 	else
 	{
 		return generateV();
