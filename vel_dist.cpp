8a36688dbc9812b529eefed292e2ffaedfa62a37
jaswantp <jaswantphysx@gmail.com>
2018-10-11 00:31:23 +0530

    commented more sections

--- vel_dist.cpp
+++ vel_dist.cpp
@@ -10,6 +10,7 @@ VelDist::VelDist()
 	v_th = 1;
 	n_0  = 200000;
 	v_b  = 4;
+	//pointers to f,v,x
 	ptr_f = new double[n_0];
 	ptr_v = new double[n_0];
 	ptr_x = new double[n_0];
@@ -36,6 +37,7 @@ int VelDist::setn_0(int n0)
 	else
 	{
 		n_0 = n0;
+		//re-allocate for new number of electrons.
 		delete ptr_f;
 		delete ptr_v;
 		delete ptr_x;
@@ -98,6 +100,8 @@ double VelDist::generateF(double v, int i)
 	double param1 = 1/(2.*pi*v_th*v_th);
 	double param2 = -(v-v_b) * (v-v_b)/(v_th * v_th * 2.);
 	double param3 = -(v+v_b) * (v+v_b)/(v_th * v_th * 2.);
+
+	//f(v) for i'th electron
 	*(ptr_f+i) = (n_0 * 0.5) * sqrt(param1) * (exp(param2) + exp(param3)) ;
 
 	//FMAX at v = +v_b or -v_b.
