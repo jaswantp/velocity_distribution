4a9126f10595eea24cf2f7de71b6d152dcfcded7
jaswantp <jaswantphysx@gmail.com>
2018-10-16 01:34:43 +0530

    overloaded filewrite to write strings

--- file_io.cpp
+++ file_io.cpp
@@ -4,11 +4,14 @@ FileIO::FileIO()
 {
 	//Open file for writing and place header row.
 	file.open("./fv.out", std::ios::trunc | std::ios::out);
-	file << "f," << "v," << "x" << std::endl;
 }
 FileIO::~FileIO()
 {
 	file.close();
+}
+void FileIO::fileWrite(char *a, char *b, char *c)
+{
+	file << a << "," << b << "," << c << std::endl;
 }
 void FileIO::fileWrite(double f, double v, double x)
 {
