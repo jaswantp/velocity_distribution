351672d7e6d28a99aaae0fd6f9e161265bb5f8eb
jaswantp <jaswantphysx@gmail.com>
2018-10-16 01:44:40 +0530

    corrected filewrite statement

--- file_io.cpp
+++ file_io.cpp
@@ -12,10 +12,10 @@ FileIO::~FileIO()
 void FileIO::fileWrite(char *a, char *b, char *c)
 {
 	//write headers
-	file << a << "," << b << "," << c << std::endl;
+	file << a << ", " << b << ", " << c << std::endl;
 }
 void FileIO::fileWrite(double f, double v, double x)
 {
 	//write data
-	file << f << "," << v << "," << x << std::endl;
+	file << f << ", " << v << ", " << x << std::endl;
 }
