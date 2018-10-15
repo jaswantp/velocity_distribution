6c34b2c5b3d1ed1a875b94c1bfd96502254b2c4e
jaswantp <jaswantphysx@gmail.com>
2018-10-16 00:05:35 +0530

    remove .cproject files and made a simpler makefile

--- clean.sh
+++ clean.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-cd ./build/make.run.linux.x86_64/ &&
-make -f ../../Makefile BUILD_MODE=run clean &&
+make clean &&
 exit
--- run.sh
+++ run.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
-if [ ! -d "./build/make.run.linux.x86_64/build" ]; then
-  mkdir -p ./build/make.run.linux.x86_64/
-fi
-cd ./build/make.run.linux.x86_64/ &&
-make -f ../../Makefile BUILD_MODE=run &&
+make &&
 ls &&
 ./vel_dist &&
-cd ../../ &&
 python3 plot.py &&
 exit
 
